21a38f5ee8c73e808d09cce560348d75ca004864
David Q. Jin <quan.jin@me.com>
2020-03-28 20:01:16 -0500

    fix postcss in prod

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
 	// Specify the paths to all of the template files in your project
-	content: ['./src/**/*.js', './public/index.html'],
+	content: [
+		'./src/**/*.js',
+		'./src/**/*.jsx',
+		'./src/**/*.ts',
+		'./src/**/*.tsx',
+		'./public/index.html'
+	],
 
 	// Include any special characters you're using in this regular expression
 	defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
