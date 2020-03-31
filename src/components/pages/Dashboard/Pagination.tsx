d005649206c013ef2d1d200235d0ab855433d9f8
David Q. Jin <quan.jin@me.com>
2020-03-30 22:07:42 -0500

    fix jsx warnings

--- src/components/pages/Dashboard/Pagination.tsx
+++ src/components/pages/Dashboard/Pagination.tsx
@@ -39,9 +39,9 @@ export default memo(function Pagination({ totalTransactions, size }: Props) {
 							>
 								<svg className='h-5 w-5' fill='currentColor' viewBox='0 0 20 20'>
 									<path
-										fill-rule='evenodd'
+										fillRule='evenodd'
 										d='M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z'
-										clip-rule='evenodd'
+										clipRule='evenodd'
 									/>
 								</svg>
 							</button>
@@ -65,9 +65,9 @@ export default memo(function Pagination({ totalTransactions, size }: Props) {
 							>
 								<svg className='h-5 w-5' fill='currentColor' viewBox='0 0 20 20'>
 									<path
-										fill-rule='evenodd'
+										fillRule='evenodd'
 										d='M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z'
-										clip-rule='evenodd'
+										clipRule='evenodd'
 									/>
 								</svg>
 							</button>
